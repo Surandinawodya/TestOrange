201e82087e6f35650a38f44ee3426d2c738779eb
Surandi Nawodya <surandidm@gmail.com>
2026-05-06 15:48:33 +0530

    Increase timeout for employee input visibility check to 30 seconds

--- pages/assignLeavePage.js
+++ pages/assignLeavePage.js
@@ -32,7 +32,7 @@ export class AssignLeavePage {
 
     await this.page.waitForLoadState('networkidle');
 
-    await expect(this.employeeInput).toBeVisible({ timeout: 20000 });
+    await expect(this.employeeInput).toBeVisible({ timeout: 30000 });
   }
 
 
